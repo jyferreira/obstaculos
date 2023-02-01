540b2eb2051740eecde5aaae7be138b672954668
jyferreira <121454300+jyferreira@users.noreply.github.com>
2023-02-01 21:09:39 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
 let faixa = false
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
-        music.playTone(494, music.beat(BeatFraction.Double))
+    basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 40 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+        music.playTone(494, music.beat(BeatFraction.Whole))
         faixa = Math.randomBoolean()
         if (faixa == true) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 144)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            basic.pause(1000)
+            basic.pause(500)
         }
         if (faixa == false) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 144)
-            basic.pause(1000)
+            basic.pause(500)
         }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
